67bf42bfe8aed1c1244228d764ebbc92b00c85ba
yamadarumi <ntdj1830@gmail.com>
2019-08-18 17:29:18 +0900

    Conflict resolution4

--- lib.js
+++ lib.js
@@ -10,11 +10,11 @@ class Creature {
     this.wait = [];
   }
   move() {
-    if (this.path.length > 0) {
+    if (this.path.length > 10) {
       // １フレーム移動
       this.p = this.path.shift();
       this.wait.push(this.p);
-    } else if (this.wait.length > 0) {
+    } else if (this.wait.length > 10) {
       // その場で待つ
       this.wait.shift();
     } else {
