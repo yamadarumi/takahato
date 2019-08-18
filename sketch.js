89a404e5e18af1cd13133ad3fc073fed272872d0
yamadarumi <ntdj1830@gmail.com>
2019-08-18 16:01:06 +0900

    Conflict resolution2

--- sketch.js
+++ sketch.js
@@ -3,13 +3,11 @@ const STEP = 65;
 const R = 100;
 const MAX_WAIT = 150;
 
-let plants = new Plants();
+let creature;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  const p = createVector(windowWidth, windowHeight).div(2); // 画面中央
-  const plant = new Plant(p);
-  plants.add(plant);
+  creature = new Creature();
 }
 
 function draw() {
